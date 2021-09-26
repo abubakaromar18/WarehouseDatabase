9c39bb064726aef8420c11673a30b3d3e2184065
chengxiong11 <cheng.xiong1@hotmail.com>
2021-09-26 12:17:39 -0500

    update

--- Client.java
+++ Client.java
@@ -5,37 +5,39 @@ import java.text.Format;
 
 public class Client{
     
-    private String name;
-    private String address;
-    private String phone;
+    private String cName;
+    private String cAddress;
+    private String cPhone;
     private String clientID;
 
     // Constructor
-    public Client(String name, String address, String phone, String clientID){
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
+    public Client(String cName, String cAddress, String cPhone, String clientID){
+        this.cName = cName;
+        this.cAddress = cAddress;
+        this.cPhone = cPhone;
         this.clientID = clientID;
     }
+
+    // Setter
+    public void getCName(String cName) { this.cName = cName; }
     
-    public String returnClientName(){
-        return name; 
-    }
+    public void getCAddress(String cAddress) { this.cAddress = cAddress; }
 
-    public String returnClientAddress(){
-        return address;
-    }
+    public void getCPhone(String cPhone) { this.cPhone = cPhone; }
 
-    public String returnClientPhone(){
-        return phone;
-    }
+    public void getCID(String cID) { this.clientID = cID; }
 
-    public String returnClientID(){
-        return clientID;
-    }
+    // Getter
+    public String returnClientcName(){ return cName; }
+
+    public String returnClientcAddress(){ return cAddress; }
+
+    public String returnClientcPhone(){ return cPhone; }
+
+    public String returnClientID(){ return clientID; }
 
     @Override
-    public String toString(){ return this.name+ "  " + this.address + " " + this.phone + "  " + this.clientID; }
+    public String toString(){ return this.cName+ "  " + this.cAddress + " " + this.cPhone + "  " + this.clientID; }
 
 }
 
