01daec4efce196682b5d5e3664f7eb1021ddc04f
Jay1625 <73314252+Jay1625@users.noreply.github.com>
2021-09-26 12:24:06 -0500

    Add files via upload

---
+++ SupplierList.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class SupplierList implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private List<Supplier> aList = new LinkedList<Supplier>();
+	private static SupplierList supplierList;
+	
+	private SupplierList() {
+	}
+	
+	public static SupplierList instance() {
+		if(supplierList == null) {
+			return (supplierList = new SupplierList());
+		} else {
+			return supplierList;
+		}
+	}
+
+	public boolean addSupplier(Supplier aSupplier) {
+		aList.add(aSupplier);
+		return true;
+	}
+	
+	public Supplier getSupplier(String anId) {
+		for(Supplier supplier: aList) {
+			if(anId.equalsIgnoreCase(supplier.getId()))
+				return (supplier);
+		}
+		return null;
+	}
+		
+	public Iterator<Supplier> getSupplierList() {
+		return aList.iterator();
+	}
+	
+	private void readObject(java.io.ObjectInputStream inPut) {
+		if(aList != null)
+			return;
+		else {
+			try {
+				inPut.defaultReadObject();
+				if(supplierList == null) {
+					supplierList = (SupplierList)inPut.readObject();
+				}
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}	
+	
+	private void writeObject(java.io.ObjectOutputStream outPut) {
+		try {
+			outPut.defaultWriteObject();
+			outPut.writeObject(supplierList);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String toString() {
+		return aList.toString();
+	}
+}
