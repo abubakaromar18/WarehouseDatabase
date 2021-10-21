e02bc8273c517331c7f8c85d82d41a5db55588bd
Jay1625 <73314252+Jay1625@users.noreply.github.com>
2021-10-21 13:48:43 -0500

    Update Supplier.java

--- Supplier.java
+++ Supplier.java
@@ -20,11 +20,11 @@ public class Supplier implements Serializable{
 	public void setid(String id) { this.id = id; }
 	public void setName(String name) { this.name = name; }
 	public void setAddress(String address) {this.address = address; }
-	public boolean AssignProduct(Product Item) {
+	public boolean AssignProduct(products Item) {
 		pList.add(Item);
 		return true;
 	}	
-	public boolean RemoveProduct(Product Item) {
+	public boolean RemoveProduct(products Item) {
 		return pList.removeProduct(Item);
 	}
 	// Getter
