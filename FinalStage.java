a61234b7c28483fff8b24e887a8e07f98ec7e656
Abubakar Omar <abubakaromar18@gmail.com>
2021-10-20 21:11:57 -0500

    4,5

--- FinalStage.java
+++ FinalStage.java
@@ -9,11 +9,29 @@ public class FinalStage {
 
 
         /* Question 3 */
+        products prod1 = new products("Pen","P1245", "Ball-point Pen", 100, 1.75 );
+        products prod2 = new products("NoteBook","P3267", "College Ruled 80 page", 200, 1.75 );
+        products prod3 = new products("Backpack","P1795", "8 Pocket Backpack", 250, 1.75 );
+        products prod4 = new products("Planners","P1345", "2021 Daily Planner", 100, 1.75 );
+        products prod5 = new products("Pencil","P1655", "No.7 Pencil", 150, 1.75 );
+        products prod6 = new products("Sharpener","P2135", "Electrical Sharpener", 50, 1.75 );
+        products prod8 = new products("Binder","P6455", "2 inch Binder", 125, 1.75 );
+        products prod9 = new products("Highlighter","P3657", "8 Pack Multi-Color", 0, 1.75 );
+        products prod10 = new products("Calculator","P4526", "TI-84 Graphing Calculator", 20, 1.75 );
 
 
         /* Question 4 */
-
-
+        productList inventory = new productList();
+        inventory.addProductList(prod1);
+        inventory.addProductList(prod2);
+        inventory.addProductList(prod3);
+        inventory.addProductList(prod4);
+        inventory.addProductList(prod5);
+        inventory.addProductList(prod6);
+        inventory.addProductList(prod7);
+        inventory.addProductList(prod8);
+        inventory.addProductList(prod9);
+        inventory.addProductList(prod10);
         /* Question 5 */
 
 
